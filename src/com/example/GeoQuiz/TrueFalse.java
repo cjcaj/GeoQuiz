0f3b2e167b8eb07b2d4ec47333e63e9bbc3e8716
carlo <carlo@ichange.com>
2014-02-26 21:19:19 -0800

    Initial commit

---
+++ src/com/example/GeoQuiz/TrueFalse.java
@@ -0,0 +1,31 @@
+package com.example.GeoQuiz;
+
+/**
+ * Created by carloca on 2/26/14.
+ */
+public class TrueFalse {
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+}
